c8a622a2d35b7d810eb87a91e991aa7b507601b3
ScherzerThomas <thomasscherzer@gmail.com>
2025-12-26 18:38:39 +0100

    changed run script

--- online-kalkulator-print-addon/run.sh
+++ online-kalkulator-print-addon/run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 cd /app
 
+echo "Searching for uvicorn"
+find / -type f -name "uvicorn" 2>/dev/null
+
+
 source /app/venv/bin/activate
+which python
+which uvicorn
+
+
 
 # Use venv python
 exec uvicorn main:app --host 0.0.0.0 --port 8000
+
